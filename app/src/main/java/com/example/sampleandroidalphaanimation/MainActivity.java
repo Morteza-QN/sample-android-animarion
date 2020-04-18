5e833b338686b6de2cacb668f68ccf274068fd91
morteza <mortezaghorbani3@gmail.com>
2020-04-18 16:36:49 +0430

    scale Animation added

--- app/src/main/java/com/example/sampleandroidalphaanimation/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidalphaanimation/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //          sample AlphaAnimation
+        //      sample AlphaAnimation       //start
         final AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
         alphaAnimation.setDuration(3000);
-        alphaAnimation.setRepeatCount(Animation.INFINITE);//repeat for ever
         alphaAnimation.setFillAfter(true);//give stage final
+        alphaAnimation.setRepeatCount(Animation.INFINITE);//repeat for ever
         alphaAnimation.setRepeatMode(Animation.REVERSE);
         alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -39,16 +40,23 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onAnimationRepeat: ");
             }
         });
+        //      sample AlphaAnimation       //end
 
-        //scale Animation
-
+        //          scale Animation         //start
+        final ScaleAnimation scaleAnimation =
+                new ScaleAnimation(1, 2, 1, 2, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(1000);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
+        scaleAnimation.setRepeatMode(Animation.REVERSE);
+        //          scale Animation         //end
 
         final ImageView imageView   = findViewById(R.id.im_main);
         View            playAnimBtn = findViewById(R.id.fab_main);
         playAnimBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(alphaAnimation);
+                imageView.startAnimation(scaleAnimation);
                 Log.i(TAG, "onClick: playAnimBtn");
             }
         });
