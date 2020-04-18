09e18c0e87c8735e9624c7bad365baa93c054f05
morteza <mortezaghorbani3@gmail.com>
2020-04-18 21:27:40 +0430

    AnimationSet added

--- app/src/main/java/com/example/sampleandroidalphaanimation/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidalphaanimation/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.sampleandroidalphaanimation;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,23 +45,51 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onAnimationRepeat: ");
             }
         });
-        //      sample AlphaAnimation       //end
 
         //          scale Animation         //start
+
         final ScaleAnimation scaleAnimation =
                 new ScaleAnimation(1, 2, 1, 2, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         scaleAnimation.setDuration(1000);
         scaleAnimation.setFillAfter(true);
         scaleAnimation.setRepeatCount(Animation.INFINITE);
         scaleAnimation.setRepeatMode(Animation.REVERSE);
-        //          scale Animation         //end
+
+        //          Transaction Animation
+
+        final TranslateAnimation translateAnimation =
+                new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0, Animation.RELATIVE_TO_PARENT, 0,
+                                       Animation.RELATIVE_TO_PARENT, 0, Animation.RELATIVE_TO_PARENT, 0.6f);
+        translateAnimation.setDuration(1500);
+        translateAnimation.setFillAfter(true);
+        //        translateAnimation.setInterpolator(new AccelerateInterpolator());//speed slow after fast
+        //        translateAnimation.setInterpolator(new DecelerateInterpolator());//speed fast after slow
+        //        translateAnimation.setInterpolator(new BounceInterpolator());//ball
+        translateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());//fast slow
+
+        //      Rotate Animation
+
+        final RotateAnimation rotateAnimation =
+                new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(1000);
+
+        //      AnimationSet
+
+        final AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setDuration(1500);
+        animationSet.setInterpolator(new AccelerateInterpolator());
+        animationSet.setFillAfter(true);
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(translateAnimation);
+
 
         final ImageView imageView   = findViewById(R.id.im_main);
         View            playAnimBtn = findViewById(R.id.fab_main);
         playAnimBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(scaleAnimation);
+                imageView.startAnimation(animationSet);
                 Log.i(TAG, "onClick: playAnimBtn");
             }
         });
